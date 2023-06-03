e50094645ea09e963f91cf92b0417811550a788c
Jayden Boyd <jaydenboydb1@gmail.com>
2023-06-03 08:56:59 -0500

    pushing

--- server.js
+++ server.js
@@ -7,22 +7,32 @@ const mongoose = require('mongoose')
 const books = require('./books')
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 
 
-app.get('/books',async (request, response) =>{
-    try {
-        const booksDos = await books.find()
-        response.json(booksDos);
-      } catch (error) {
-        console.error('Error retrieving books:', error);
-        response.status(500).json({ error: 'Server error' });
-      }
+app.get('/books', async (request, response) => {
+  try {
+    const booksDos = await books.find()
+    response.json(booksDos);
+  } catch (error) {
+    console.error('Error retrieving books:', error);
+    response.status(500).json({ error: 'Server error' });
+  }
 
+})
+app.post('/books', async (request, response) => {
+  try {
+    const bookPull = request.body
+    await books.insertMany(bookPull)
+    response.send(bookPull)
+  } catch (error) {
+
+  }
 })
 
 
-
-app.listen(3001,() => {
-    console.log(`Server Running on ${process.env.PORT}`)
+app.listen(3001, () => {
+  console.log(`Server Running on ${process.env.PORT}`)
 })
+
